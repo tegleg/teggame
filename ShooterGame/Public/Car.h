80561a9b8f0ca07614cb8d5acd53c4b7150257bd
tegleg <tegleg@hotmail.co.uk>
2015-08-28 15:51:27 +0200

    car and pickup classes

---
+++ ShooterGame/Public/Car.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/WheeledVehicle.h"
+#include "Car.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERGAME_API ACar : public AWheeledVehicle
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ ShooterGame/Public/CarWheel.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Vehicles/VehicleWheel.h"
+#include "CarWheel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERGAME_API UCarWheel : public UVehicleWheel
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
