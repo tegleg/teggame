711afc6dbf82db67d167c3770bdd3fc4efde8748
tegleg <tegleg@hotmail.co.uk>
2015-09-02 22:41:57 +0200

    added staticmeshcomponent to pickupactor

--- ShooterGame/Private/PickupActor.cpp
+++ ShooterGame/Private/PickupActor.cpp
@@ -5,9 +5,30 @@
 
 
 // Sets default values
-APickupActor::APickupActor()
+APickupActor::APickupActor(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	//Asset, Reference Obtained Via Right Click in Editor
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> StaticMeshOb_AW2(TEXT("StaticMesh'/Game/'"));
+
+	AssetSM_JoyControl = StaticMeshOb_AW2.Object;
+
+	//Create
+	JoyfulControl = ObjectInitializer.CreateDefaultSubobject < UStaticMeshComponent >(this, TEXT("JoyfulControlYay"));
+
+	//Set Mesh
+	JoyfulControl->SetStaticMesh(AssetSM_JoyControl);
+
+	//Setup (see .h)
+	SetupSMComponentsWithCollision(JoyfulControl);
+
+	//Deferred Attachment (Ty Nick W.! Actual attach gets done after blueprint stuff)
+	JoyfulControl->AttachParent = RootComponent;
+	JoyfulControl->AttachSocketName = "JoyControl";
+
+	//JoyfulControl->AttachParent = RootComponent;
+	
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
--- ShooterGame/Public/PickupActor.h
+++ ShooterGame/Public/PickupActor.h
@@ -8,7 +8,7 @@
 UCLASS()
 class SHOOTERGAME_API APickupActor : public AActor
 {
-	GENERATED_BODY()
+	GENERATED_UCLASS_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
@@ -20,6 +20,27 @@ public:
 	// Called every frame
 	virtual void Tick( float DeltaSeconds ) override;
 
-	
+	//JoyControl
+	UStaticMeshComponent* JoyfulControl;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	UStaticMesh * AssetSM_JoyControl;
+
+	FORCEINLINE void SetupSMComponentsWithCollision(UStaticMeshComponent* Comp)
+	{
+		if (!Comp) return;
+		//~~~~~~~~
+
+		Comp->bOwnerNoSee = false;
+		Comp->bCastDynamicShadow = true;
+		Comp->CastShadow = true;
+		Comp->BodyInstance.SetObjectType(ECC_WorldDynamic);
+		Comp->BodyInstance.SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		Comp->BodyInstance.SetResponseToAllChannels(ECR_Ignore);
+		Comp->BodyInstance.SetResponseToChannel(ECC_WorldStatic, ECR_Block);
+		Comp->BodyInstance.SetResponseToChannel(ECC_WorldDynamic, ECR_Block);
+		Comp->BodyInstance.SetResponseToChannel(ECC_Pawn, ECR_Block);
+		Comp->SetHiddenInGame(false);
+	}
 	
 };
